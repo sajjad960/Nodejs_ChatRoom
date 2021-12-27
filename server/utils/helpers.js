f00e9182d222aa8ee9e315da081a7ddf254207bf
sajjad960 <sajjadhossainsb01@gmail.com>
2021-12-27 17:16:50 +0600

    after creating Users on helpers

---
+++ server/utils/helpers.js
@@ -0,0 +1,14 @@
+let users = [];
+const addUser = ({socket_id, name, user_id, room_id}) => {
+    const exist = users.find(user => user.room_id === room_id && user.user_id === user_id);
+
+    if(exist) {
+        return {error: 'User already exist in this room'}
+    }
+
+    const user = {socket_id, name, user_id, room_id};
+    users.push(user);
+    return {user:user}
+}
+
+module.exports = {addUser}
